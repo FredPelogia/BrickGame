e37efc107f51582000a03bce64959698007ff1e4
Fred Pelogia <fredpelogia@outlook.com>
2020-10-03 22:49:45 -0300

    upload fpelogia.github.io version here

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ function setup() {
   plSh = new ship(1);
   // aiship = new ship(0);  
   aiship.push(new ship(0));
-  img = loadImage("https://i.imgur.com/rxe4Vnn.png");
+  img = loadImage("Images/brick_bg.png");
   
 }
 
